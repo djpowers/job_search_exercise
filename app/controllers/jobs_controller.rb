2e079923dc9cf3243ca94a3e167dc305fd254df2
Dave Powers <djpowers89@gmail.com>
2014-11-12 22:17:22 -0500

    Send GET request from controller to pull in jobs via API. Display jobs by company.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,7 @@
 class JobsController < ApplicationController
   def index
-    
+    uri = URI.parse("http://localhost:4000/jobs")
+    response = Net::HTTP.get_response(uri)
+    @jobs = JSON.parse(response.body)
   end
 end
