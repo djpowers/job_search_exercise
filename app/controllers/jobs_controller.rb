9b1fab4ff47bc8a0a31a2d7c60b3d117d1ddf538
Dave Powers <djpowers89@gmail.com>
2014-11-12 23:15:20 -0500

    Move API call into private method to remove duplication

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,15 +1,19 @@
 class JobsController < ApplicationController
   def index
-    uri = URI.parse("http://localhost:4000/jobs")
-    response = Net::HTTP.get_response(uri)
-    @jobs = JSON.parse(response.body)
+    @jobs = call_jobs_api
     @jobs.sort! { |x,y| y["posted"] <=> x["posted"] }
   end
 
   def show
+    jobs = call_jobs_api
+    @job = jobs[params[:id].to_i - 1]
+  end
+
+  private
+
+  def call_jobs_api
     uri = URI.parse("http://localhost:4000/jobs")
     response = Net::HTTP.get_response(uri)
-    @jobs = JSON.parse(response.body)
-    @job = @jobs[params[:id].to_i - 1]
+    JSON.parse(response.body)
   end
 end
