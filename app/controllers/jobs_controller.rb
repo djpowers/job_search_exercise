a6193759ae2cfafdd0c18c527dee1c4725662665
Dave Powers <djpowers89@gmail.com>
2014-11-12 23:11:06 -0500

    Add show action for jobs and display company name in view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,11 @@ class JobsController < ApplicationController
     @jobs = JSON.parse(response.body)
     @jobs.sort! { |x,y| y["posted"] <=> x["posted"] }
   end
+
+  def show
+    uri = URI.parse("http://localhost:4000/jobs")
+    response = Net::HTTP.get_response(uri)
+    @jobs = JSON.parse(response.body)
+    @job = @jobs[params[:id].to_i - 1]
+  end
 end
