c03435fd69dc9d93a39d95bb4005dd496ea90074
Dave Powers <djpowers89@gmail.com>
2014-11-12 22:30:04 -0500

    Sort jobs by posted time and display formatted date

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,5 +3,6 @@ class JobsController < ApplicationController
     uri = URI.parse("http://localhost:4000/jobs")
     response = Net::HTTP.get_response(uri)
     @jobs = JSON.parse(response.body)
+    @jobs.sort! { |x,y| y["posted"] <=> x["posted"] }
   end
 end
